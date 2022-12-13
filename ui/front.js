4e37e38deb10589e07c633237a95a293bd2a5c72
JarkkoUtriainen <jarkko.utriainen@metropolia.fi>
2022-12-13 17:25:00 +0200

    css

--- ui/front.js
+++ ui/front.js
@@ -1,36 +1,44 @@
-'use strict'
+"use strict";
 
 //Normal get functiom
-async function getAPI(url){
-const result = await fetch(url, {
-    method: 'GET',
-    credentials: 'include'
-  })
-  return result.json()
+async function getAPI(url) {
+  const result = await fetch(url, {
+    method: "GET",
+    credentials: "include",
+  });
+  return result.json();
 }
 
 //Post function
-async function postAPI(url = '', data = {}) {
+async function postAPI(url = "", data = {}) {
   // Default options are marked with *
   const response = await fetch(url, {
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'include',
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "include",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
-    body: JSON.stringify(data)
-  })
-  return response.json()
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(data),
+  });
+  return response.json();
 }
 
-function flyImage() {
-  let image = document.getElementById("window-image")
+async function sleep(seconds) {
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+}
 
-  image.src = '/ui/images/runway.jpg'
-  image.src = '/ui/images/sky.jpg'
-  image.src = '/ui/images/decent.jpg'
+async function flyImage() {
+  let image = document.getElementById("window-image");
+
+  image.src = "/ui/images/runway.jpg";
+  await sleep(1);
+  image.src = "/ui/images/sky.jpg";
+  await sleep(1);
+  image.src = "/ui/images/decent.jpg";
+  await sleep(1);
+  image.src = "/ui/images/hangar.jpg";
 }
